f2173855985a5e974d8f4a9565abf686343fa571
Justin Benson <jbenson.dev@gmail.com>
2019-12-16 08:53:49 -0800

    Update installDocker.sh

--- installDocker.sh
+++ installDocker.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #install docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
