a1536bdf6562fcafea0ec6bd62af77414db1972f
Justin Benson <jbenson.dev@gmail.com>
2020-02-23 21:26:56 -0800

    Update dotNetCore.sh

--- scripts/programs/dotNetCore.sh
+++ scripts/programs/dotNetCore.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #install .NET Core SDK
 wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
