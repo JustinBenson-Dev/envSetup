8e2bdc9982d144514ff9e23b7a37366766b7a2d6
Justin Benson <jbenson.dev@gmail.com>
2020-02-23 20:51:04 -0800

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./symlink.sh
+./aptinstall.sh
+./programs.sh
+./desktop.sh
+
+# Get all upgrades
+sudo apt update && sudo apt upgrade -y
+
+# See our bash changes
+source ~/.bashrc
+
+# Fun hello
