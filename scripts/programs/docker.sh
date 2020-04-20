1af47f186082499c2c1387491567476f4889f0c6
Justin Benson <jbenson.dev@gmail.com>
2020-04-20 16:01:00 -0700

    Update docker.sh

--- scripts/programs/docker.sh
+++ scripts/programs/docker.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 #Uninstall old versions of docker
-echo "#Uninstall old versions of docker"
 sudo apt remove docker docker-engine docker.io containerd runc
 sudo apt purge docker-ce docker-ce-cli containerd.io -y
 
 #run Ubuntu updates 
-echo "#run Ubuntu updates"
 sudo apt update && sudo apt upgrade -y
 
 #1. SET UP THE REPOSITORY 
-echo "#1. SET UP THE REPOSITORY"
 sudo apt install -y \
     apt-transport-https \
     ca-certificates \
@@ -19,31 +16,25 @@ sudo apt install -y \
     software-properties-common
     
 #2. Add Docker’s official GPG key:   
-echo "#2. Add Docker’s official GPG key:"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 #3. Set up docker stable repo.
-echo "#3. Set up docker stable repo."
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+   
 #run updates again
-echo "#run updates again"
 sudo apt update
 
 #4. Install docker engine
-echo "#4. Install docker engine"
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 #clean up
-echo "clean up installation"
 sudo apt autoremove
 
 #5. Use Docker as a non-root user
-echo "#5. Use Docker as a non-root user"
 sudo usermod -aG docker $USER
 
 #closing notes
-echo "#closing notes"
-echo "BE SURE TO SIGN OUT then sign back in to run as non-root. Confirm using: docker run hello-world to test."
+echo "NOTES: BE SURE TO SIGN OUT then sign back in to run as non-root. Confirm using: docker run hello-world to test."
