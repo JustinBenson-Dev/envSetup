2461726021f9439ca9236e9e5ca67821fe2c7a27
Justin Benson <jbenson.dev@gmail.com>
2020-05-14 15:32:56 -0700

    Create powertop.sh

---
+++ scripts/programs/powertop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#on Ubuntu based distros
+
+sudo apt install libpci-dev libnl-3-dev libnl-genl-3-dev gettext libgettextpo-dev autopoint gettext libncurses5-dev libncursesw5-dev libtool-bin dh-autoreconf autoconf-archive pkg-config
+
+wget https://01.org/sites/default/files/downloads//powertop-2.12.tar.gz
+
+gunzip powertop-2.12.tar.gz
+
+cd powertop-2.12
+
+./autogen.sh
+./configure
+make
+sudo make install
