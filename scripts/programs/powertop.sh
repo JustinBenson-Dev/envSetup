92e58cfe7d7a47d080d156d5f1bddfdba31a49fc
Justin Benson <jbenson.dev@gmail.com>
2020-05-14 16:31:14 -0700

    Update powertop.sh

--- scripts/programs/powertop.sh
+++ scripts/programs/powertop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #on Ubuntu based distros
+#run this from ~/ directory for cd into powertop-2.12 to work
 
 sudo apt install libpci-dev libnl-3-dev libnl-genl-3-dev gettext libgettextpo-dev autopoint gettext libncurses5-dev libncursesw5-dev libtool-bin dh-autoreconf autoconf-archive pkg-config
 
@@ -8,7 +9,7 @@ wget https://01.org/sites/default/files/downloads//powertop-2.12.tar.gz
 
 gunzip powertop-2.12.tar.gz
 
-cd powertop-2.12
+cd powertop-2.12 #run this from ~/ directory for cd into powertop-2.12 to work
 
 ./autogen.sh
 ./configure
