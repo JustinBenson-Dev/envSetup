e84e15056fd1a161741d39d27fcd23f7184934c5
Justin Benson <jbenson.dev@gmail.com>
2020-05-15 08:13:30 -0700

    Update teamviewer.sh

--- scripts/programs/teamviewer.sh
+++ scripts/programs/teamviewer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Install old version of TeamViewer (12)
+
 #use the 32/64 multiArchitecture download.  The 64bit doesn't work (unresolveable dependencies).
 wget https://download.teamviewer.com/download/version_12x/teamviewer_i386.deb
 
