b95ba1649a50feae572233f85e3c717013a8a31e
Justin Benson <jbenson.dev@gmail.com>
2020-05-15 07:50:39 -0700

    Update teamviewer.sh

--- scripts/programs/teamviewer.sh
+++ scripts/programs/teamviewer.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-wget https://download.teamviewer.com/download/version_12x/teamviewer_amd64.deb
-sudo apt install ./teamviewer_amd64.deb -y
+wget https://download.teamviewer.com/download/version_12x/teamviewer_amd64.deb | sudo apt install ./teamviewer_amd64.deb -y
