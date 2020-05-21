5a8f0af7a8b8ed99b9ea0a32b471e17d0febef59
Justin Benson <jbenson.dev@gmail.com>
2020-05-21 14:57:20 -0700

    Update brave-browser-beta.sh

--- scripts/programs/brave-browser-beta.sh
+++ scripts/programs/brave-browser-beta.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #set up brave browser beta channel
 
+echo "Installing Brave Browser"
+
 sudo apt install apt-transport-https curl
-curl -s https://brave-browser-apt-beta.s3.brave.com/brave-core-nightly.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-prerelease.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
 sudo apt update
-sudo apt install brave-browser-beta
+sudo apt install brave-browser
