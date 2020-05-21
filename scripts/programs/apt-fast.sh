feddb2e6495629aa0e5454fccc580395075e03ad
Justin Benson <jbenson.dev@gmail.com>
2020-05-21 16:29:12 -0700

    Update apt-fast.sh

--- scripts/programs/apt-fast.sh
+++ scripts/programs/apt-fast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Installing apt-fast"
-sudo add-apt-repository ppa:apt-fast/stable
+sudo add-apt-repository ppa:apt-fast/stable -y
 sudo apt-get update
 sudo apt-get install apt-fast -y
