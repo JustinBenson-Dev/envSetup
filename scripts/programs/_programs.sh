63f846c2d8101e4ba7492fe11f44def811989c30
Justin Benson <jbenson.dev@gmail.com>
2020-05-21 09:33:56 -0700

    Update _programs.sh

--- scripts/programs/_programs.sh
+++ scripts/programs/_programs.sh
@@ -5,21 +5,21 @@
 ./teamviewer.sh
 ./brave-browser-beta.sh
 #./customdock-gnome.sh
-./exfat.sh
 ./bashtop.sh
 
 #install tools
 sudo apt install curl -y  #install curl
 sudo apt install htop -y  #install htop for resource monitoring
 sudo apt install wget unzip -y  #install wget unzip
-#sudo apt install tmux -y  #install tmux  
+sudo apt install tmux -y  #install tmux  
 sudo apt install terminator -y  #install terminator
 sudo apt install nmap -y  #install nmap
 sudo apt install git -y  #install git
-sudo apt install gthumb
+sudo apt install gthumb -y
+sudo apt install exfat-fuse exfat-utils -y
 
 #apps
-sudo apt install gnome-boxes
+sudo apt install gnome-boxes -y
 
 #install snaps
 sudo snap install docker #install Docker
