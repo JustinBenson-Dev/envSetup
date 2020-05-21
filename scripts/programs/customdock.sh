bf8a62b3d95807ab9f202672372e9523e3e0c64a
Justin Benson <jbenson.dev@gmail.com>
2020-05-21 07:25:44 -0700

    Update customdock.sh

--- scripts/programs/customdock.sh
+++ scripts/programs/customdock.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo apt -y install dconf-editor
-
 dconf-editor
+
+# go to "org > gnome > shell > extensions > dash-to-dock" 
+# search for "show-apps-at-top" and set it to "on"
+# this will set the "Show Applications" button to the far left on the dock
