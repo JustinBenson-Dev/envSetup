512325e3e5ec60022a336e6831631768799f15fd
Justin Benson <jbenson.dev@gmail.com>
2020-05-21 14:45:02 -0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-./symlink.sh
+#update Ubuntu and get standard apt repo programs
 ./aptinstall.sh
+
+#get snaps
+./snapinstall.sh
+
+#Run all programs / install scripts
+for f in programs/*.sh; do bash "$f" -H; done
 ./programs.sh
 ./desktop.sh
 
-# Get all upgrades
-sudo apt update && sudo apt upgrade -y
+#Get all upgrades
+sudo apt upgrade -y
 
-# See our bash changes
-#source ~/.bashrc
-
-# Fun hello
+#See our bash changes
+source ~/.bashrc
