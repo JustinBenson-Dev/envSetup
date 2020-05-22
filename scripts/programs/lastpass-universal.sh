1c380fae390a6727730a47dfc249f036b9f68e64
Justin Benson <jbenson.dev@gmail.com>
2020-05-22 15:53:05 -0700

    Update lastpass-universal.sh

--- scripts/programs/lastpass-universal.sh
+++ scripts/programs/lastpass-universal.sh
@@ -3,13 +3,16 @@
 #download and install LastPass Universal Linux Installer
 
 echo "Installing LastPass Universal Linux Installer..."
-
-curl --remote-name https://download.cloud.lastpass.com/linux/lplinux.tar.bz2 https://download.cloud.lastpass.com/linux/lplinux.tar.bz2
+mkdir lastpass
+cd lastpass
+curl --remote-name https://download.cloud.lastpass.com/linux/lplinux.tar.bz2
 
 tar xjvf lplinux.tar.bz2
 
 ./install_lastpass.sh
 
- rm -rf lplinux.tar.bz2
- 
- #source:  https://lastpass.com/lplinux.php
+cd ..
+
+rm -rf ./lastpass
+  
+#source:  https://lastpass.com/lplinux.php
