5df9ebf094f2a9f0c7896a3089061d31130554a2
Justin Benson <jbenson.dev@gmail.com>
2020-05-22 13:06:38 -0700

    Create lastpass-universal.sh

---
+++ scripts/programs/lastpass-universal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#download and install LastPass Universal Linux Installer
+
+echo "Installing LastPass Universal Linux Installer..."
+
+curl -s https://download.cloud.lastpass.com/linux/lplinux.tar.bz2
+
+tar xjvf lplinux.tar.bz2
+
+./install_lastpass.sh
+
+ rm -rf lplinux.tar.bz2
+ 
+ #source:  https://lastpass.com/lplinux.php
