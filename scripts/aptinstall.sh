8006a12ac6d059f19a3ba840e6e3d62e02597d24
Justin Benson <jbenson.dev@gmail.com>
2020-05-27 13:18:47 -0700

    Update aptinstall.sh

--- scripts/aptinstall.sh
+++ scripts/aptinstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo apt update
+sudo apt-fast update
 
 function install {
   which $1 &> /dev/null
 
   if [ $? -ne 0 ]; then
     echo "Installing: ${1}..."
-    sudo apt install -y $1
+    sudo apt-fast install -y $1
   else
     echo "Already installed: ${1}"
   fi
