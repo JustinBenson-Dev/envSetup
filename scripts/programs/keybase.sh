20b884abe85d5bfe0400d97d36938365cb6f5a1a
Justin Benson <jbenson.dev@gmail.com>
2020-05-27 13:14:29 -0700

    Update keybase.sh

--- scripts/programs/keybase.sh
+++ scripts/programs/keybase.sh
@@ -3,6 +3,6 @@
 echo "Installing Keybase..."
 
 curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
-sudo apt install -y ./keybase_amd64.deb
+sudo apt-fast install -y ./keybase_amd64.deb
 run_keybase
 rm -rf keybase_amd64.deb
