a76b7053453b5f6ffd8b6f04e391a00c2ac5e7f2
Justin Benson <jbenson.dev@gmail.com>
2020-05-27 13:01:32 -0700

    Update brave-browser.sh

--- scripts/programs/brave-browser.sh
+++ scripts/programs/brave-browser.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Installing Brave Browser..."
-sudo apt install apt-transport-https curl -y
+sudo apt-fast install apt-transport-https curl -y
 
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-sudo apt update
-sudo apt install brave-browser -y
+sudo apt-fast update
+sudo apt-fast install brave-browser -y
