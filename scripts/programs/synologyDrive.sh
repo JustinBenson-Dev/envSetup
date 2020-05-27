6ec5ceff3768f2f7fe9885549a3278b7264e12bf
Justin Benson <jbenson.dev@gmail.com>
2020-05-27 13:16:45 -0700

    Update synologyDrive.sh

--- scripts/programs/synologyDrive.sh
+++ scripts/programs/synologyDrive.sh
@@ -5,5 +5,5 @@
 echo "Installing Synology Drive client..."
 
 curl --remote-name "https://global.download.synology.com/download/Tools/SynologyDriveClient/2.0.2-11078/Ubuntu/Installer/x86_64/synology-drive-client-11078.x86_64.deb"
-sudo apt install -y ./synology-drive-client-11078.x86_64.deb
+sudo apt-fast install -y ./synology-drive-client-11078.x86_64.deb
 rm -rf synology-drive-client-11078.x86_64.deb
