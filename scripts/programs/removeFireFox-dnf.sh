de16985f668dd4a4b2010cedfe35debaefc56f9f
Justin Benson <jbenson.dev@gmail.com>
2020-05-29 11:57:34 -0700

    Update and rename removeFireFox.sh to removeFireFox-dnf.sh

--- scripts/programs/removeFireFox-dnf.sh
+++ scripts/programs/removeFireFox-dnf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #remove Firefox browser
-sudo apt purge firefox -y
+sudo dnf purge firefox -y
 sudo rm -rf /home/.mozilla .adobe
 sudo rm -rf /etc/firefox
 sudo rm -rf /usr/lib/firefox firefox-addons
