687f611011e5a1262824bac5d569b0a871a28714
Justin Benson <jbenson.dev@gmail.com>
2020-05-29 11:24:51 -0700

    Update and rename docker.sh to docker-dnf.sh

---
+++ scripts/programs/docker-dnf.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "Installing docker..."
+
+#Uninstall old versions of docker
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+#set up local Docker repo
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+
+#install Docker engine
+sudo dnf install -y docker-ce docker-ce-cli containerd.io
+
+#cgroups exception
+sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
+
+#reboot then start Docker with systemctl
+sudo systemctl start docker
+
+#optional test install if you don't care about sudo use
+#sudo docker run hello-world
+
+#post install to add to docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+#it's best to log out here, but you can run this too:
+newgrp docker 
+#test running Docker
+docker run hello-world
+
+#enable Docker start at boot
+sudo systemctl enable docker
+#disable start at boot
+#sudo systemctl disable docker
