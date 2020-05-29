34a5a4426531085225509b2b41f529729fd60589
Justin Benson <jbenson.dev@gmail.com>
2020-05-29 12:24:39 -0700

    Create caprine-dnf.sh

---
+++ scripts/programs/caprine-dnf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#WIP WIP WIP
+
+cd ./appimages
+
+curl --remote-name "https://github.com/sindresorhus/caprine/releases/download/v2.46.0/Caprine-2.46.0.AppImage"
+chmod +x ./Caprine-2.46.0.AppImage
+
+cd ..
