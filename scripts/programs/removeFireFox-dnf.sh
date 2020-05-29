4ade5a5952e86b36d8c68f43fbbcdcd3ea6c169b
Justin Benson <jbenson.dev@gmail.com>
2020-05-29 15:07:49 -0700

    Update removeFireFox-dnf.sh

--- scripts/programs/removeFireFox-dnf.sh
+++ scripts/programs/removeFireFox-dnf.sh
@@ -3,6 +3,3 @@
 #remove Firefox browser
 sudo dnf remove firefox.x86_64 -y
 sudo dnf clean all
-sudo rm -rf /home/.mozilla .adobe
-sudo rm -rf /etc/firefox
-sudo rm -rf /usr/lib/firefox firefox-addons
