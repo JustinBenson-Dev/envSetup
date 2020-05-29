b9fa38b9056bdd1b2f727b36aebdefd82dcea8f0
Justin Benson <jbenson.dev@gmail.com>
2020-05-29 15:13:42 -0700

    Update docker-dnf.sh

--- scripts/programs/docker-dnf.sh
+++ scripts/programs/docker-dnf.sh
@@ -15,7 +15,7 @@ sudo dnf remove -y docker \
                   docker-engine
 
 #set up local Docker repo
-sudo dnf -y install dnf-plugins-core
+sudo dnf install -y dnf-plugins-core
 sudo dnf config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
