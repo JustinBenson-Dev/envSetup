2cf40405a61d975f2569d8bc134b533f1e0cf67a
Justin Benson <jbenson.dev@gmail.com>
2020-05-29 16:38:33 -0700

    Update teamviewer-dnf.sh

--- scripts/programs/teamviewer-dnf.sh
+++ scripts/programs/teamviewer-dnf.sh
@@ -4,10 +4,10 @@ echo "Installing TeamViewer 12..."
 
 #Fedora: Install old version of TeamViewer (12)
 #use the 32/64 multiArchitecture download.  The 64bit doesn't work (unresolveable dependencies).
-curl --remote-name https://download.teamviewer.com/download/version_12x/teamviewer.i686.rpm
+wget https://download.teamviewer.com/download/version_12x/teamviewer.i686.rpm
 
 #install .deb file using apt
-sudo dnf -y install ./teamviewer.i686.rpm
+sudo dnf -y install teamviewer.i686.rpm
 sudo rm teamviewer.i686.rpm*
 
 #source: https://www.teamviewer.com/en/download/previous-versions/
