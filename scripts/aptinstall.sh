3b625e3162e0fc6daf78587c1682b4b160c46056
Justin Benson <jbenson.dev@gmail.com>
2020-05-29 10:24:41 -0700

    Update aptinstall.sh

--- scripts/aptinstall.sh
+++ scripts/aptinstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo apt update
+sudo dnf upgrade -y
 
 function install {
   which $1 &> /dev/null
 
   if [ $? -ne 0 ]; then
     echo "Installing: ${1}..."
-    sudo apt install -y $1
+    sudo dnf -y $1
   else
     echo "Already installed: ${1}"
   fi
