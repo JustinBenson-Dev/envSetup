a45e3fc78940acf244a8239ee0eb79f9622e6d43
Justin Benson <jbenson.dev@gmail.com>
2020-05-29 18:40:40 -0700

    Update zoom-client-dnf.sh

--- scripts/programs/zoom-client-dnf.sh
+++ scripts/programs/zoom-client-dnf.sh
@@ -2,5 +2,5 @@
 
 echo "Installing Zoom client..."
 wget https://zoom.us/client/latest/zoom_x86_64.rpm
-sudo dnf localinstall zoom_x86_64.rpm
+sudo dnf -y localinstall zoom_x86_64.rpm
 rm -f zoom_x86_64.rpm
