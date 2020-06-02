9cd5bd6a704d9dbdbba0f0baf4d96507267e074f
Justin Benson <jbenson.dev@gmail.com>
2020-06-02 10:44:34 -0700

    Update desktop.sh

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -3,4 +3,5 @@
 # Set up the desktop environment
 cp ../img/desktop.jpg ~/desktop.jpg
 #cp ../img/lock.jpg ~/lock.jpg
-su - jabs -c dconf load /org/gnome/ < ./dconf.settings
+#load dconf settings as user "jabs" 
+su - $USER -c "dconf load /org/gnome/ < ./dconf.settings"
