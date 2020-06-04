7ce7ebd726d1ac67c19fbd67322220fa7f4ccef8
Justin Benson <jbenson.dev@gmail.com>
2020-06-04 08:19:36 -0700

    Update remmina.sh

--- scripts/programs/remmina.sh
+++ scripts/programs/remmina.sh
@@ -6,4 +6,4 @@ echo "Installing Remmina..."
 
 sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next
 sudo apt-fast update
-sudo apt install -y remmina remmina-plugin-rdp remmina-plugin-secret
+sudo apt-fast install -y remmina remmina-plugin-rdp remmina-plugin-secret
