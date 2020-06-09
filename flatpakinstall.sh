2334bb0540e2764599aa09e41b586b8224e65c50
Justin Benson <jbenson.dev@gmail.com>
2020-06-09 09:35:14 -0700

    Create flatpakinstall.sh

---
+++ flatpakinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo flatpak install -y $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+#install flatpaks
+install flathub 
+install us.zoom.Zoom
+install org.gabmus.hydrapaper
