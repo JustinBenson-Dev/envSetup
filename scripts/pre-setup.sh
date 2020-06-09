9c77a62a356f0c57dc73fa16c09b55e918d6871a
Justin Benson <jbenson.dev@gmail.com>
2020-06-09 14:59:39 -0700

    Update pre-setup.sh

--- scripts/pre-setup.sh
+++ scripts/pre-setup.sh
@@ -2,5 +2,5 @@
 
 sudo apt install -y git && git clone -b lmde4 https://github.com/bensonlabs/dotfiles
 sudo chmod +x ./dotfiles -R
-echo alias ll='ls -lh' >> ~/.bashrc
+echo "alias ll='ls -lh'" >> ~/.bashrc
 source ~/.bashrc
