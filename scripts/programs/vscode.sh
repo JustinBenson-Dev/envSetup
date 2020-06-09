3ee784b874c62b08d24d2ed789c7084afdc2ebcc
Justin Benson <jbenson.dev@gmail.com>
2020-06-09 08:31:36 -0700

    Create vscode.sh

---
+++ scripts/programs/vscode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#install vscode
+echo "Installing Visual Studio Code..."
+
+wget https://go.microsoft.com/fwlink/?LinkID=760868
+curl --remote-name https://go.microsoft.com/fwlink/?LinkID=760868
