d0c1b92ab9c8b2daa5976c41cabca19561bc1695
Justin Benson <jbenson.dev@gmail.com>
2020-06-09 09:08:43 -0700

    Update vscode.sh

--- scripts/programs/vscode.sh
+++ scripts/programs/vscode.sh
@@ -3,5 +3,11 @@
 #install vscode
 echo "Installing Visual Studio Code..."
 
-wget https://go.microsoft.com/fwlink/?LinkID=760868
-curl --remote-name https://go.microsoft.com/fwlink/?LinkID=760868
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+#Then update the package cache and install the package using:
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get -y install code 
