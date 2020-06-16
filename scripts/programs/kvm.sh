019a7b9d8129eaa6b9344fc8248edb01e43bb40a
Justin Benson <jbenson.dev@gmail.com>
2020-06-15 20:37:53 -0700

    Update kvm.sh

--- scripts/programs/kvm.sh
+++ scripts/programs/kvm.sh
@@ -3,5 +3,6 @@
 echo "Installing KVM..."
 
 sudo apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager
+sudo apt-get install -y virt-viewer
 
 #https://linuxconfig.org/install-and-set-up-kvm-on-ubuntu-20-04-focal-fossa-linux
