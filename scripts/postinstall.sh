9c97920cdb998ad2e365bebb7bbbe9e4704671ad
Justin Benson <jbenson.dev@gmail.com>
2020-06-18 11:04:35 -0700

    Update postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,5 +8,16 @@ sudo systemctl mask cups
 #To review active sercices, run the following command
 sudo systemctl list-units | grep service
 
+#https://wiki.centos.org/HowTos/Laptops/Wireless
+
 #To get wifi going, install NetworkManager-wifi
 sudo yum install -y NetworkManager-wifi
+
+#Check status of NetworkManager service:
+sudo systemctl status NetworkManager.service
+
+#check wifi device list
+sudo nmcli dev
+sudo nmcli r wifi
+
+# wifisupplicant
