c832cdc1ad033621195c8aa29d5c36aa1d29456f
Justin Benson <jbenson.dev@gmail.com>
2020-06-18 10:01:04 -0700

    Update postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,5 +6,7 @@ sudo systemctl enable firewalld
 sudo systemctl mask cups
 
 #To review active sercices, run the following command
-
 sudo systemctl list-units | grep service
+
+#To get wifi going, install NetworkManager-wifi
+sudo yum install -y NetworkManager-wifi
