330fe8477987f5f5b95ced3ef2796f78746a6557
Justin Benson <jbenson.dev@gmail.com>
2020-06-18 09:42:28 -0700

    Update postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,3 +4,7 @@ sudo yum update -y
 sudo systemctl start firewalld
 sudo systemctl enable firewalld
 sudo systemctl mask cups
+
+#To review active sercices, run the following command
+
+sudo systemctl list-units | grep service
