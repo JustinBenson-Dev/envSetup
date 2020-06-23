2537439d16d3fee95186a7bb2f54bd3d01911cf1
Justin Benson <jbenson.dev@gmail.com>
2020-06-23 12:28:14 -0700

    Update bashtop.sh

--- scripts/programs/bashtop.sh
+++ scripts/programs/bashtop.sh
@@ -2,8 +2,16 @@
 
 echo "Installing Bashtop..."
 
-echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
-wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
-
+echo "Installing Bashtop..."
+sudo add-apt-repository ppa:bashtop-monitor/bashtop -y #install #bashtop
 sudo apt update
-sudo apt install bashtop -y 
+sudo apt install bashtop -y
+
+
+#old non PPA method
+#
+#echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
+#wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
+
+#sudo apt update
+#sudo apt install bashtop -y 
