1227877cb123f2d951dd144f065f28f346a98be2
Justin Benson <jbenson.dev@gmail.com>
2020-06-23 12:30:09 -0700

    Update bashtop.sh

--- scripts/programs/bashtop.sh
+++ scripts/programs/bashtop.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "Installing Bashtop..."
-
-echo "Installing Bashtop..."
 sudo add-apt-repository ppa:bashtop-monitor/bashtop -y #install #bashtop
 sudo apt update
 sudo apt install bashtop -y
